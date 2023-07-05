e2d6c72c477275cef87154e2433c8218d40157a6
xiayudev <jctuesta7@gmail.com>
2023-07-05 15:50:10 -0700

    fix: do nothing when pressing ENTER key

--- console.py
+++ console.py
@@ -8,16 +8,24 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter"""
+    intro = "Welcome to the command interpreter for AirBnB clone"
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
         "Quit command to exit the program"
+        print()
         return True
 
     def do_EOF(self, arg):
         """Capture EOF (Ctrl + D)"""
         print("Exiting...")
         return True
+
+    def postcmd(self, stop, line):
+        """Execute after processing each command"""
+        if line == 'EOF':
+            return True
+        self.lastcmd = ""
 
 
 if __name__ == '__main__':
